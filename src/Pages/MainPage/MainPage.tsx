79dea5f4760c625653a7d4179236d01c9b725701
Nils Wyss <nils.wyss@ardevia.ch>
2024-08-27 22:30:57 +0200

    Added responsivness for smaller screens max 400px

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import { Button, Grid, Box } from "@mui/material";
-import "@fontsource/rozha-one";
+import { Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import InstaPromotion from "../../Components/InstaPromotion/InstaPromotion";
 import "./mainPage.css";
@@ -9,9 +8,9 @@ import "./mainPage.css";
 const MainPage = () => {
   return (
     <>
-      <Box className="root">
+      <Box className="mainContainer">
         {/* Image Section */}
-        <Box className="imageSection">
+        <Box className="imageContainer">
           <img
             src={require("../../page_assets/WelcomeGroup.jpg")}
             alt="Books"
@@ -20,41 +19,28 @@ const MainPage = () => {
         </Box>
 
         {/* Text Section */}
-        <Box className="textSection">
-          <Box className="welcomeBox">
-            <Typography
-              className="titleText"
-              fontFamily="rozha-one"
-              variant="h4"
-              gutterBottom
-            >
-              THEBOOKCLUB ETH
-            </Typography>
-            <Typography
-              variant="h6"
-              fontFamily="rozha-one"
-              className="titleSubtitle"
-              gutterBottom
-            >
-              Welcome to the first book club at ETH! You might be thinking,
-              "Book club... what could that be?" Well, we're not your typical
-              book club. Our goal is to create a community and a platform where
-              students can meet fellow readers. No qualifications are needed to
-              consider yourself a reader! Anyone is welcome, maybe you read 500
-              pages a day (...is that possible as a student though?) or the last
-              time you read was when you were 14 years old. As long as you have
-              a passion for reading, we would love to have you!
-            </Typography>
-            <Button
-              variant="contained"
-              className="exploreButton"
-              component={Link}
-              to="/events"
-              size="large"
-            >
-              Explore Upcoming Events
-            </Button>
-          </Box>
+        <Box className="textContainer">
+          <Typography className="titleText" variant="h4" gutterBottom>
+            THEBOOKCLUB ETH
+          </Typography>
+          <Typography variant="body1" className="titleSubtitle" gutterBottom>
+            Welcome to the first book club at ETH! You might be thinking, "Book
+            club... what could that be?" Well, we're not your typical book club.
+            Our goal is to create a community and a platform where students can
+            meet fellow readers. No qualifications are needed to consider
+            yourself a reader! Anyone is welcome, whether you read 500 pages a
+            day or the last time you read was when you were 14 years old. As
+            long as you have a passion for reading, we would love to have you!
+          </Typography>
+          <Button
+            variant="contained"
+            className="exploreButton"
+            component={Link}
+            to="/events"
+            size="large"
+          >
+            Explore Upcoming Events
+          </Button>
         </Box>
       </Box>
       <InstaPromotion />
