ee3e1fa85eb57e8a97b4757c7d269c30fc082ed9
Nils Wyss <nils.wyss@ardevia.ch>
2024-08-27 17:16:49 +0200

    Test for pipline after small change

--- src/Pages/AboutUsPage/AboutUsPage.tsx
+++ src/Pages/AboutUsPage/AboutUsPage.tsx
@@ -92,7 +92,7 @@ const AboutUsPage: React.FC = () => {
             className="teamDescription"
           >
             Emilia Mende - Founder & President <br />
-            Selin .... - Event Coordinator <br />
+            Selin .... - Founder & President <br />
           </Typography>
         </Box>
       </Box>
